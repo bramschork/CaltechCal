6651081803b95276022e8a0dd9d75eaf71f0d77a
Bram Schork <bramschork99@gmail.com>
2022-11-28 00:29:41 -0500

    program now parses REGIS HTML file

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 '''
 Author: Bram Schork
-Date: September 2022
+Date: November 2022
 
-Version: 1.0.1
+Version: 2.0.0
+Academic Year: 2022-23
 
 Version Notes: 
- - Currently setup for fall term 2022
- - Program allows for spaces in the days of the week
- - Program input only accepts CSV files as the input in the file dialogue menu
- - Added error messages
+ - No longer need to generate CSV file, the program can parse the HTML file from Caltech Access
  
  ToDo:
-  - Error out for inproperly formatted/headered input CSV file
   - Make file dialogue menu pop to top of other windows
+  - Get current date and use that to figure out which start date to use
+  - Check for null/nan values. Ex. for recitation
+  - Check access link
 '''
 
-# FALL TERM 2022 DATES
-start_date = '09-27-2022'
-end_date = '12-02-2022'
-days_off_raw = ['11-24-2022', '11-25-2022']
+# 2022-23 Academic Year Dates
+start_dates = ['09-27-2022', '01-04-2023', '04-03-2023'] # beginning of instruction
+end_dates = ['12-02-2022', '03-10-2023', '06-09-2023'] # last day of classes
+days_off_raw = ['11-24-2022', '11-25-2022', '01-16-2023', '02-20-2023', '05-29-2023']
 
 ### IMPORTS ###
 import pandas as pd
@@ -26,8 +26,7 @@ import datetime
 import calendar
 import tkinter as tk
 from tkinter import filedialog
-import os
-import sys
+import os, sys
 
 # Set environment variable to silence TK
 os.environ['TK_SILENCE_DEPRECATION'] = '1'
@@ -40,8 +39,9 @@ root.lift()
 root.withdraw()
 
 # Get class file from user
-path = filedialog.askopenfilename(filetypes=(("CSV Files","*.csv"),))
+path = filedialog.askopenfilename(filetypes=(("HTML Files","*.html"),))
 
+# Error out if no file is given
 while path == '':
     print('No file entered. Exiting.')
     sys.exit(-1)
@@ -50,16 +50,11 @@ while path == '':
 # Turn date strings into datetime objects
 start_date = datetime.datetime.strptime(start_date, '%m-%d-%Y')
 end_date = datetime.datetime.strptime(end_date, '%m-%d-%Y')
+
+# Reformat days_off_raw
 days_off = []
 for item in days_off_raw:
-       days_off.append(datetime.datetime.strptime(item, '%m-%d-%Y'))
-
-# Convert class file into Pandasa DataFrame
-try:
-    df = pd.read_csv(path)
-except FileNotFoundError:
-    print('Error reading file. Exiting.')
-    sys.exit(-1)
+    days_off.append(datetime.datetime.strptime(item, '%m-%d-%Y'))
 
 # Create empty DataFrame for the output
 outputDF = pd.DataFrame()
@@ -74,10 +69,31 @@ days_of_week = {
 }
 
 
-# check_date - This is a var that I will iterate through to check every day of the term
+def getDOW(vals):  
+    '''
+    getDOW takes in a takes of values and converts their abbreviations to their full names. Ex. TR --> Tuesday.
+    It does this using the days_of_week dictionary.
+    The function returns a list contians the full names in the same order as they came in.
+    '''  
+    i = 0
+    DOW = []
+    while i < len(vals):
+        if vals[i] != 'T' and vals[i] != 'R':
+            DOW.append(vals[i])
+        else:
+            if vals[i] == 'R':
+                pass
+            elif vals[i + 1] == 'R':
+                DOW.append('TR')
+            else:
+                DOW.append('T')
+        i += 1
+    return DOW
+
+# check_date is a var that I will iterate through to check every day of the term
 check_date = start_date
 
-# Iterate over every single class
+# Create empty lists to store the class information for calendar import
 course_names = []
 descriptions = []
 dates = []
@@ -85,36 +101,69 @@ start_time = []
 end_time = []
 locations = []
 
+# Read the class HTML data as a Pandas object
+table = pd.read_html(path)
+
+# Reformat the Pandas dataframe from 1 list to a normal Pandas dataframe
+df = table[0].dropna(axis=0, thresh=4)
+
+
 for index, row in df.iterrows():
+    
+    # Start adding classes on the first day of the term
     check_date = start_date
-    # For each class, iterate through each date to add a calendar event for every class
-    class_days = row['Class Days'].replace(' ', '')
-    class_days = [days_of_week[item] for item in class_days.split(',')]
+    
+    # Class days is a list containing the full DOW of days I have class
+    class_days = []
+    line = row['Days/Time'] #Ex. MTR 11:00-11:55 W 14:00-14:55
+    line = line.split(' ')
+    abbreviated_names = []
+    class_times = []
+    all_class_days = []
+    
+    i = 0
+    while i < len(line):        
+        # If not numeric, meaning it is a DOW (do this by checking first character)
+        if not line[i][0].isnumeric():
+            abbreviated_names.append([getDOW(line[i]), line[i + 1]])
+        i += 1
         
+    for item in abbreviated_names:
+        for day in item[0]:
+            all_class_days.append(days_of_week[day])
+            class_days.append([days_of_week[day], item[1]])
+    # make all class days final item of list
+    class_days.append(all_class_days)
+
     while check_date <= end_date:
-        # dow = Day of Week
+        # Get full DOW from check_date
         dow = (calendar.day_name[check_date.weekday()])
         
         if dow != 'Saturday' and dow != 'Sunday' and check_date not in days_off:
             
             # if the check date is a day I have class, create an event
-            if dow in class_days:
+            if dow in class_days[-1]:
                 # Add event title
-                course_names.append(row['Course Name'])
+                course_names.append(row['Offering Name'])
                 
                 # Add event description
-                descriptions.append('Course Title: {0}\nSection/Instructor: {1}'.format(row['Course Title'], row['Section/Instructor']))
+                section = row['Section/Instructor'][0:2]
+                instructor = row['Section/Instructor'][3:]
+                units = row['Units']
+                descriptions.append('Section: {}\nInstructor: {}\nUnits: {}\n\nOffering Title & Class Info:\n{}\n'.format(section, instructor, units, row['Offering Title']))
 
                 # Add start date
                 dates.append(check_date)
                 
-                # Add Start Time
-                times = row['Class Time'].split('-')
+                # Add Class Time
+                for item in class_days[0:-1]:
+                    if item[0] == dow:
+                        times = item[1].split('-')
                 start_time.append(times[0])
                 end_time.append(times[1])
                 
                 # Add Location
-                locations.append(row['Class Location'])
+                locations.append(row['Location'])
                 
         check_date += datetime.timedelta(days=1)
             
@@ -124,5 +173,5 @@ outputDF['Start Date'] = dates
 outputDF['Start  Time'] = start_time
 outputDF['End Time'] = end_time
 outputDF['Location'] = locations
-outputDF.to_csv('CaltechCalOutput.csv')
+outputDF.to_csv('CaltechCalOutput.csv', index=False)
 print('Output File Generated')
